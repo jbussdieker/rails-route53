7fdfd814f77cf92e74ee7c9a140b59c1f954a96a
Joshua Bussdieker <jbussdieker@gmail.com>
2013-09-06 13:04:02 -0400

    Better sync functions for hosted zones and health checks

--- app/models/health_check.rb
+++ app/models/health_check.rb
@@ -1,9 +1,10 @@
 class HealthCheck < ActiveRecord::Base
-  attr_accessible :fully_qualified_domain_name, :health_check_id, :ip_address, :port, :resource_path, :type
+  attr_accessible :health_check_id, :fully_qualified_domain_name, :ip_address, :port, :resource_path, :type
   self.inheritance_column = nil
 
-  def self.each_health_check(&block)
+  def self.list_health_checks
     client = AWS::Route53::Client.new
+    list = []
     done = false
     next_marker = nil
     while done == false
@@ -11,7 +12,7 @@ class HealthCheck < ActiveRecord::Base
       options.merge!(:marker => next_marker) if next_marker
       resp = client.list_health_checks(options)
       resp[:health_checks].each do |health_check|
-        yield(health_check)
+        list << health_check
       end
       if resp[:is_truncated]
         next_marker = resp[:next_marker]
@@ -19,14 +20,32 @@ class HealthCheck < ActiveRecord::Base
         done = true
       end
     end
+    list
   end
 
   def self.sync
-    each_health_check do |health_check|
-      record = {}
-      record.merge!(:health_check_id => health_check[:id])
-      record.merge!(health_check[:health_check_config])
-      create(record)
+    local_list = all
+    remote_list = list_health_checks
+
+    # Create missing local ones
+    remote_list.each do |health_check|
+      if local_list.find {|item| item.health_check_id == health_check[:id]}
+      else
+        record = {}
+        record.merge!(:health_check_id => health_check[:id])
+        record.merge!(health_check[:health_check_config])
+        create(record)
+      end
     end
+
+    # Delete missing remote ones
+    local_list.each do |health_check|
+      if remote_list.find {|item| item[:id] == health_check.health_check_id}
+      else
+        health_check.destroy
+      end
+    end
+
+    nil
   end
 end
--- app/models/hosted_zone.rb
+++ app/models/hosted_zone.rb
@@ -1,13 +1,49 @@
 class HostedZone < ActiveRecord::Base
   attr_accessible :hosted_zone_id, :name
 
-  def self.sync
-    r53 = AWS::Route53.new
-    r53.hosted_zones.each do |zone|
-      hosted_zone = find_by_zone_id(zone.id)
-      if !hosted_zone
-        create(:zone_id => zone.id, :name => zone.name)
+  def self.list_hosted_zones
+    client = AWS::Route53::Client.new
+    list = []
+    done = false
+    next_marker = nil
+    while done == false
+      options = {}
+      options.merge!(:marker => next_marker) if next_marker
+      resp = client.list_hosted_zones(options)
+      resp[:hosted_zones].each do |hosted_zone|
+        list << hosted_zone
+      end
+      if resp[:is_truncated]
+        next_marker = resp[:next_marker]
+      else
+        done = true
       end
     end
+    list
+  end
+
+  def self.sync
+    local_list = all
+    remote_list = list_hosted_zones
+
+    # Create missing local ones
+    remote_list.each do |hosted_zone|
+      if local_list.find {|item| item.hosted_zone_id == hosted_zone[:id]}
+      else
+        record = {:hosted_zone_id => hosted_zone[:id]}
+        record.merge!(:name => hosted_zone[:name])
+        create(record)
+      end
+    end
+
+    # Delete missing remote ones
+    local_list.each do |hosted_zone|
+      if remote_list.find {|item| item[:id] == hosted_zone.hosted_zone_id}
+      else
+        hosted_zone.destroy
+      end
+    end
+
+    nil
   end
 end
