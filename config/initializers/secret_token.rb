9fa5ddcd65904b8426407b0b9aa442fc18eafb6f
Joshua Bussdieker <jbussdieker@gmail.com>
2013-09-06 11:25:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Route53::Application.config.secret_token = 'ac1ee34c4330e6573ec3d9bddee8b5a4d39e11b063a938402d0474607311a319e5952426637dbd7d45d4b21e0d561136a633bd88fdbfd7477289a55557d41561'
