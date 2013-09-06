61f8246d83c5e03c8fe112be407a11da361dca2d
Joshua Bussdieker <jbussdieker@gmail.com>
2013-09-06 11:35:34 -0400

    Sync hosted zones with AWS

--- app/models/hosted_zone.rb
+++ app/models/hosted_zone.rb
@@ -1,3 +1,13 @@
 class HostedZone < ActiveRecord::Base
   attr_accessible :name, :zone_id
+
+  def self.sync
+    r53 = AWS::Route53.new
+    r53.hosted_zones.each do |zone|
+      hosted_zone = find_by_zone_id(zone.id)
+      if !hosted_zone
+        create(:zone_id => zone.id, :name => zone.name)
+      end
+    end
+  end
 end
